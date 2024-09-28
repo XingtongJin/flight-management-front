b8db5941fc68fd8492039d24c416bb05eda00970
XingtongJin <3474263198@qq.com>
2024-09-29 00:58:44 +1200

    Login and Register template

---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="login_container">
+      <div class="login_box">
+        <!--表单提交区域-->
+        <el-form :rules="loginFormRules" ref="loginFormRef" label-width="0px" class="login_form" :model="loginForm">
+          <!--用户名-->
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user" placeholder="username" clearable></el-input>
+          </el-form-item>
+          <!--密码-->
+          <el-form-item prop="password">
+            <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima" placeholder="password" show-password clearable></el-input>
+          </el-form-item>
+          <!--按钮区-->
+          <el-form-item class="btns">
+            <el-checkbox class="remember" v-model="keepPassword">记住密码</el-checkbox>
+            <br/>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="primary" @click="register">注册</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </template>
