b5dd01d209e1fad8d63df04813dc9f0ec225be61
XingtongJin <3474263198@qq.com>
2024-10-02 00:39:35 +1300

    add homepage navi header

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="mainpage">
+    <NaviHeader></NaviHeader>
+    <router-view></router-view>
+  </div>
+</template>
+
+
+<script>
+import NaviHeader from '@/components/navi/NaviHeader.vue'
+export default {
+  name: 'HomePage',
+  components: { NaviHeader },
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
