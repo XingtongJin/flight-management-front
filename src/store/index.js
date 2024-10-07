a2bdf79c067b630cfa439c9219d6a94fbe680ecf
XingtongJin <3474263198@qq.com>
2024-10-08 01:48:39 +1300

    add order function to flight page

--- src/store/index.js
+++ src/store/index.js
@@ -10,12 +10,28 @@ const actions = {
 
 // mutations, 操作数据
 const mutations = {
-
+    PUTFLIGHTINFO(state, flightInfo) {
+        state.orderFlightInfo = flightInfo
+    },
+    PUTFLIGHTTYPE(state, seatType) {
+        state.seatType = seatType
+    },
+    PUTSEATNUMBER(state, seatNumber) {
+        // console.log("mutation seatNumber: " + seatNumber)
+        state.seatNumber = seatNumber
+    }
 }
 
 // state， 存储数据
 const state = {
-
+    // 订单页面 
+      //订单信息
+      orders: [],
+      //订单信息
+      orderFlightInfo: [],
+      //经济舱 E，商务舱 B
+      seatType: 'E',
+      seatNumber: ''
 }
 
 // 创建store
